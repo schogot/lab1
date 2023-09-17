3af4296403bccab2174efc30add88c7e1523195e
schogot <135862678+schogot@users.noreply.github.com>
2023-09-17 17:52:28 +0300

    Add files via upload

---
+++ LB1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int min;
+    int max;
+    int const arraySize = 5;
+    int mass[arraySize] = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << mass[i] << " ";
+    }
+    cout << "\n";
+
+    max = mass[0];
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (mass[i] > max)
+            max = mass[i];
+    }
+
+    min = mass[0];
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (mass[i] < min)
+            min = mass[i];
+    }
+
+    cout << "Максимальный элемент = " << max << "\n"
+        << "Минимальный элемент = " << min << "\n"
+        << "Разность' = " << max - min << endl;
+
+    return 0;
+}
---
+++ LB5A.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <locale.h> 
+#include <Windows.h>
+
+struct student 
+{ 
+char famil[20]; 
+char name[20]; 
+char facult[20]; 
+int Nomzach; 
+}; 
+struct student findStudent(struct student* stud, int size, const char* queryFamil, const char* queryName, const char* queryFacult)
+{ 
+	for (int i = 0; i < size; i++) { 
+		if (strcmp(stud[i].famil, queryFamil) == 0 && strcmp(stud[i].name, queryName) == 0 && strcmp(stud[i].facult, queryFacult) == 0)
+		{ 
+			return stud[i]; } } 
+	struct student notFoundStudent; 
+	strcpy(notFoundStudent.famil, "Not Found"); 
+	strcpy(notFoundStudent.name, "Not Found"); 
+	strcpy(notFoundStudent.facult, "Not Found"); 
+	notFoundStudent.Nomzach = -1; 
+	return notFoundStudent; } 
+int main() 
+{ 
+setlocale(LC_ALL, "Russian");
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+const int size = 3; 
+struct student students[size]; 
+for (int i = 0; i < size; i++) 
+{ 
+	printf("Введите фамилию студента %d: ", i + 1); 
+	scanf("%s", students[i].famil); 
+	printf("Введите имя студента %d: ", i + 1); 
+	scanf("%s", students[i].name); 
+	printf("Введите факультет студента %d: ", i + 1); 
+	scanf("%s", students[i].facult); 
+	printf("Введите номер зачетки студента %d: ", i + 1); 
+	scanf("%d", &students[i].Nomzach); } 
+		char queryFamil[20]; 
+		char queryName[20]; 
+		char queryFacult[20]; 
+	printf("\nВведите фамилию для поиска: "); 
+	scanf("%s", queryFamil); 
+	printf("Введите имя для поиска: "); 
+	scanf("%s", queryName); 
+	printf("Введите факультет для поиска: "); 
+	scanf("%s", queryFacult); 
+struct student foundStudent = findStudent(students, size, queryFamil, queryName, queryFacult);
+if (strcmp(foundStudent.famil, "Not Found") == 0) 
+{ 
+	   printf("Студент не найден\n"); 
+} 
+else { 
+	   printf("\nНайден студент:\n"); 
+       printf("Фамилия: %s\n", foundStudent.famil); 
+	   printf("Имя: %s\n", foundStudent.name); 
+	   printf("Факультет: %s\n", foundStudent.facult); 
+	   printf("Номер зачетки: %d\n", foundStudent.Nomzach); 
+} 
+return 0; 
+}
